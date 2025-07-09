ca0997981b66d84d17e4562313fb3eb7d2084f1d
Alexander Ampleev <ampleyan@gmail.com>
2025-07-09 23:43:50 +0200

    Updates

---
+++ .eslintsrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'prettier',
+    '@vue/prettier'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-v-html': 'off',
+    'vue/require-default-prop': 'error',
+    'vue/require-prop-types': 'error',
+    'vue/order-in-components': ['error', {
+      order: [
+        'el',
+        'name',
+        'key',
+        'parent',
+        'functional',
+        ['delimiters', 'comments'],
+        ['components', 'directives', 'filters'],
+        'extends',
+        'mixins',
+        ['provide', 'inject'],
+        'ROUTER_GUARDS',
+        'layout',
+        'middleware',
+        'validate',
+        'scrollToTop',
+        'transition',
+        'loading',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'emits',
+        'setup',
+        'data',
+        'computed',
+        'watch',
+        'created',
+        'mounted',
+        'methods',
+        ['template', 'render'],
+        'renderError'
+      ]
+    }]
+  }
+};
